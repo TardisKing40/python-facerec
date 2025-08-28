ef21d8b3d8a99df4a8e66dc3131fa19c8ab5cd4f
TardisKing40 <tardisking40@gmail.com>
2025-08-28 11:50:42 +1000

    add comments for understanding

--- facerec.py
+++ facerec.py
@@ -1,16 +1,25 @@
+# Get basic requirements
 import cv2
 import face_recognition
 
+# Set default video capture
 video_capture = cv2.VideoCapture(0)
 
+# Run continuously 
 while True:
+    # Read the current frame
     frame = video_capture.read()[1]
+    # Get the location of each face on the screen
     face_locations = face_recognition.face_locations(frame)
-
+    
+    # For every face
     for face_location in face_locations:
+        # Set their individual left,right,up,down
         top, right, bottom, left = face_location
+        # Set a rectangle outline
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
+    # Display video to camera
     cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
@@ -18,5 +27,5 @@ while True:
         break
 
 # Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+video_capture.release() # Stop capturing camera
+cv2.destroyAllWindows() # Destroy window
